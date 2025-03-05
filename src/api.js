1b5806c776802f518ac1b93de4afb0b5432f8f20
Engineed <teamengineed@gmail.com>
2025-03-05 17:44:19 +0800

    maintenance

--- src/api.js
+++ src/api.js
@@ -1,3 +1,15 @@
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const isDevelopment = import.meta.env.MODE === "development";
+const myBaseUrl = isDevelopment
+  ? import.meta.env.VITE_API_URL_LOCAL
+  : import.meta.env.VITE_API_URL_DEPLOY;
+
+const api = axios.create({
+  baseURL: myBaseUrl,
+});
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
@@ -23,3 +35,6 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+
+export default api;
